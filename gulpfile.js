000e294836edb5dc38ac0760d821bda26f9695fd
Zvi "Viz" Effron <viz@flippedperspective.com>
2015-10-01 04:05:44 -0700

    Add autoprefixer to the build pipeline
    Rather than manually add prefixes, we'll let autoprefixer generate
    them for us. It does a good job of only including the prefixes
    necessary.

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
   gulp.src('./src/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(autoprefixer(['> 1%', 'last 2 version']))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('./src/'));
 });
