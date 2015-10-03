b45b8798e6fe1516ffb8677cc4ca75d048fe98a9
Zvi "Viz" Effron <zeffron@riotgames.com>
2015-10-03 04:04:03 -0700

    Switch to TypeScript from Javascript
    This commit is only a start at the transition. The gulpfule changes
    are very brittle. Webpack was introduced because TypeScript uses
    require. The webpack implementation is incredibly brittle. It might be
    worth investigating switching to webpack as the primary build system
    and just using gulp as the task runner.

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ let watching = false;
 
 // Global tasks
 
-let allSources = './src/**/*';
+let allSources = 'src/**/*';
 
 gulp.task('build', ['compile', 'lint'])
 gulp.task('build:watch', ['set-watching'], function() {
@@ -28,22 +28,71 @@ gulp.task('set-watching', function() {
   watching = true;
 });
 
+// webpack
+
+gulp.task('webpack', function() {
+  gulp.src('src/5e/character-sheet.js')
+    .pipe(plugins.webpack({
+      output: {
+        filename: '[name].pack.js'
+      }
+    }))
+    .pipe(gulp.dest('src/5e/'));
+});
+
+// TypeScript
+
+let typescriptSources = ['src/**/*.ts', 'src/**/*.tsx'];
+
+gulp.task('typescript:compile', function() {
+  for (let glob of typescriptSources) {
+    let typescriptOptions = {
+      module: 'commonjs',
+      moduleResolution: 'node'
+    };
+    if (glob.split('.')[1] === 'tsx') {
+      typescriptOptions.jsx = 'react';
+    }
+
+    gulp.src(glob)
+      .pipe(plugins.sourcemaps.init())
+      .pipe(plugins.typescript(typescriptOptions))
+      .pipe(plugins.sourcemaps.write())
+      .pipe(gulp.dest('src/'));
+  }
+});
+gulp.task('typescript:compile:watch', ['set-watching'], function() {
+  gulp.watch(typescriptSources, ['typescript:compile']);
+});
+
+gulp.task('typescript', ['typescript:compile']);
+gulp.task('typescript:watch', ['set-watching'], function() {
+  gulp.watch(typescriptSources, ['typescript'])
+});
+
 // JSX and ES6
 
+let jsxSources = 'src/**/*.jsx';
+
 gulp.task('jsx:compile', function() {
-  gulp.src('./src/**/*.jsx')
+  gulp.src(jsxSources)
     .pipe(plugins.sourcemaps.init())
     .pipe(plugins.babel({whitelist: ['react', 'es6.blockScoping', 'es6.spread']}))
     .pipe(plugins.sourcemaps.write())
-    .pipe(gulp.dest('./src/'));
+    .pipe(gulp.dest('src/'));
 });
 gulp.task('jsx:watch', ['set-watching'], function() {
-  gulp.watch('./src/**/*.jsx', ['jsx:compile']);
+  gulp.watch(jsxSources, ['jsx:compile']);
+});
+
+gulp.task('jsx', ['jsx:compile']);
+gulp.task('jsx:watch', ['set-watching'], function() {
+  gulp.watch(jsxSources, ['jsx'])
 });
 
 // SASS
 
-let sassSources = './src/**/*.scss';
+let sassSources = 'src/**/*.scss';
 
 gulp.task('sass:compile', function() {
   gulp.src(sassSources)
@@ -51,7 +100,7 @@ gulp.task('sass:compile', function() {
     .pipe(plugins.sass({'outputStyle': 'expanded'}).on('error', plugins.sass.logError))
     .pipe(plugins.autoprefixer(['> 1%', 'last 2 version']))
     .pipe(plugins.sourcemaps.write())
-    .pipe(gulp.dest('./src/'));
+    .pipe(gulp.dest('src/'));
 });
 gulp.task('sass:compile:watch', ['set-watching'], function() {
   gulp.watch(sassSources, ['sass:compile']);
---
+++ src/5e/character-sheet.tsx
@@ -0,0 +1,114 @@
+/// <reference path="../typings/react/react.d.ts" />
+import React = require('react');
+
+let data = {
+  "character-name": "Wade Wilson",
+  "player-name": "Chris Laubach",
+  "notes": "Test",
+  domNodes: []
+}
+
+class Background extends React.Component<{}, {}> {
+  render() {
+    return (
+      <img src={this.props.src} className="background" />
+    )
+  }
+  props: {
+    src: string;
+  }
+}
+
+interface HeaderFieldProps {
+  name: string
+  label: string
+  line: string
+}
+
+class HeaderField extends React.Component<HeaderFieldProps, {}> {
+  handleChange(event) {
+    data[this.props.name] = event.target.value;
+    for (let node of data.domNodes) {
+      node.forceUpdate();
+    }
+  }
+  componentDidMount(){
+    data.domNodes.unshift(this)
+  }
+  render() {
+    return (
+      <fieldset name={this.props.name}>
+        <label>
+          {this.props.label}<span className="line">{this.props.line}</span>
+        </label>
+        <input type="text" value={data[this.props.name]} onChange={this.handleChange.bind(this)} ref="input" />
+      </fieldset>
+    );
+  }
+}
+
+class Header extends React.Component<{}, {}> {
+  render() {
+    return (
+      <header className="name">
+        <HeaderField name='character-name' label={'CHARACTER'} line={'______________________'} />
+        <HeaderField name='player-name' label={'PLAYER'} line={'_________________________'} />
+        <HeaderField name='notes' label={'NOTES'} line={'__________________________'} />
+      </header>
+    );
+  }
+}
+
+class Footer extends React.Component<{}, {}> {
+  render() {
+    return (
+      <footer className="copyright">
+        © 2013 The Topps Company, Inc. Shadowrun is a registered trademark of The Topps Company, Inc., in the United States and/or other countries. Permission given to copy for personal use.
+      </footer>
+    );
+  }
+}
+
+class CharacterSheetPageContent extends React.Component<{}, {}> {
+  render() {
+    return (
+      <div className="pdf-page-content">
+        <Background src={this.props.background} />
+        <Header />
+        <Footer />
+      </div>
+    );
+  }
+  props: {
+    background: string
+  }
+}
+
+class CharacterSheetPage extends React.Component<{}, {}> {
+  render() {
+    return (
+      <div id={this.props.id} className="pdf-page">
+        <CharacterSheetPageContent background={this.props.id + '.png'} />
+      </div>
+    );
+  }
+  props: {
+    id: string
+  }
+}
+
+class CharacterSheet extends React.Component<{}, {}> {
+  render() {
+    return (
+      <div className="pdf">
+        <CharacterSheetPage id='character-sheet-front' />
+        <CharacterSheetPage id='character-sheet-back' />
+      </div>
+    );
+  }
+}
+
+React.render(
+  <CharacterSheet />,
+  document.getElementById('character-sheet')
+);
