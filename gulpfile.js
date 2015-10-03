d0ca7820084fa27281ab36637853e510679e5ebc
Zvi "Viz" Effron <zeffron@riotgames.com>
2015-10-02 22:15:37 -0700

    Polish the scss and gulpfile
    Refactor the scss file to use a mixin to generate the mixins for units
    for the different media types.
    Refactor the gulpfile to make the tasks more explicit about what they
    do in preparation for more types of tasks (such as linters and unit
    tests).

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,23 @@
 'use strict';
 
-var gulp = require('gulp');
-var plugins = require('gulp-load-plugins')();
+let gulp = require('gulp');
+let plugins = require('gulp-load-plugins')();
 
-gulp.task('sass', function () {
-  gulp.src('./src/**/*.scss')
-    .pipe(plugins.sourcemaps.init())
-    .pipe(plugins.sass({outputStyle: 'expanded'}).on('error', plugins.sass.logError))
-    .pipe(plugins.autoprefixer(['> 1%', 'last 2 version']))
-    .pipe(plugins.sourcemaps.write())
-    .pipe(gulp.dest('./src/'));
+let allSources = './src/**/*';
+
+gulp.task('build', ['compile'])
+gulp.task('build:watch', function() {
+  gulp.watch(allSources, ['build'])
 });
 
-gulp.task('babel', function() {
+gulp.task('compile', ['babel:compile', 'sass:compile']);
+gulp.task('compile:watch', function() {
+  gulp.watch(allSources, ['compile'])
+});
+
+// JSX and ES6
+
+gulp.task('babel:compile', function() {
   gulp.src('./src/**/*.jsx')
     .pipe(plugins.sourcemaps.init())
     .pipe(plugins.babel({whitelist: ['react', 'es6.blockScoping', 'es6.spread']}))
@@ -20,10 +25,29 @@ gulp.task('babel', function() {
     .pipe(gulp.dest('./src/'));
 })
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./src/**/*.scss', ['sass']);
+gulp.task('babel:watch', function() {
+  gulp.watch('./src/**/*.jsx', ['babel:compile']);
+})
+
+// SASS
+
+let sassSources = './src/**/*.scss';
+
+gulp.task('sass:compile', function() {
+  gulp.src(sassSources)
+    .pipe(plugins.sourcemaps.init())
+    .pipe(plugins.sass({'outputStyle': 'expanded'}).on('error', plugins.sass.logError))
+    .pipe(plugins.autoprefixer(['> 1%', 'last 2 version']))
+    .pipe(plugins.sourcemaps.write())
+    .pipe(gulp.dest('./src/'));
 });
 
-gulp.task('babel:watch', function() {
-  gulp.watch('./src/**/*.jsx', ['babel']);
+gulp.task('sass:compile:watch', function() {
+  gulp.watch(sassSources, ['sass:compile']);
+});
+
+gulp.task('sass', ['sass:compile'])
+
+gulp.task('sass:watch', function() {
+  gulp.watch(sassSources, ['sass'])
 })
