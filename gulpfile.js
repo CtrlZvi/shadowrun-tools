1c6a6b3a9af122f1a78f81d53bbeb4e8d4c9951f
Zvi "Viz" Effron <zeffron@riotgames.com>
2015-10-03 17:30:33 -0700

    Switch the build process to using webpack
    In an attempt to simplify things, I'm moving the build process to use
    webpack as the build engine and gulp simply as the task engine. This
    has the benefit of allowing us to use the webpack-dev-server to
    improve iteration times.

--- gulpfile.js
+++ gulpfile.js
@@ -1,123 +1,80 @@
-'use strict';
+var gulp = require("gulp");
+var gutil = require("gulp-util");
+var webpack = require("webpack");
+var WebpackDevServer = require("webpack-dev-server");
+var webpackConfig = require("./webpack.config.js");
 
-let gulp = require('gulp');
-let plugins = require('gulp-load-plugins')();
+// The development server (the recommended option for development)
+gulp.task("default", ["webpack-dev-server"]);
 
-let watching = false;
-
-// Global tasks
-
-let allSources = 'src/**/*';
-
-gulp.task('build', ['compile', 'lint'])
-gulp.task('build:watch', ['set-watching'], function() {
-  gulp.watch(allSources, ['build'])
+// Build and watch cycle (another option for development)
+// Advantage: No server required, can run app from filesystem
+// Disadvantage: Requests are not blocked until bundle is available,
+//               can serve an old app on refresh
+gulp.task("build-dev", ["webpack:build-dev"], function() {
+    gulp.watch(["app/**/*"], ["webpack:build-dev"]);
 });
 
-gulp.task('compile', ['jsx:compile', 'sass:compile']);
-gulp.task('compile:watch', ['set-watching'], function() {
-  gulp.watch(allSources, ['compile'])
+// Production build
+gulp.task("build", ["webpack:build"]);
+
+gulp.task("webpack:build", function(callback) {
+    // modify some webpack config options
+    var myConfig = Object.create(webpackConfig);
+    myConfig.plugins = myConfig.plugins.concat(
+        new webpack.DefinePlugin({
+            process: {
+                env: {
+                    // This has effect on the react lib size
+                    "NODE_ENV": JSON.stringify("production")
+                }
+            }
+        })
+    );
+
+    // run webpack
+    webpack(myConfig, function(err, stats) {
+        if(err) throw new gutil.PluginError("webpack:build", err);
+        gutil.log("[webpack:build]", stats.toString({
+            colors: true
+        }));
+        callback();
+    });
 });
 
-gulp.task('lint', ['sass:lint']);
-gulp.task('lint:watch', ['set-watching'], function() {
-  gulp.watch(allSources, ['lint'])
+// modify some webpack config options
+var myDevConfig = Object.create(webpackConfig);
+myDevConfig.devtool = "sourcemap";
+myDevConfig.debug = true;
+
+// create a single instance of the compiler to allow caching
+var devCompiler = webpack(myDevConfig);
+
+gulp.task("webpack:build-dev", function(callback) {
+    // run webpack
+    devCompiler.run(function(err, stats) {
+        if(err) throw new gutil.PluginError("webpack:build-dev", err);
+        gutil.log("[webpack:build-dev]", stats.toString({
+            colors: true
+        }));
+        callback();
+    });
 });
 
-gulp.task('set-watching', function() {
-  watching = true;
-});
+gulp.task("webpack-dev-server", function(callback) {
+    // modify some webpack config options
+    var myConfig = Object.create(webpackConfig);
+    myConfig.devtool = "eval";
+    myConfig.debug = true;
 
-// webpack
-
-gulp.task('webpack', function() {
-  gulp.src('src/5e/character-sheet.js')
-    .pipe(plugins.webpack({
-      output: {
-        filename: '[name].pack.js'
-      }
-    }))
-    .pipe(gulp.dest('src/5e/'));
-});
-
-// TypeScript
-
-let typescriptSources = ['src/**/*.ts', 'src/**/*.tsx'];
-
-gulp.task('typescript:compile', function() {
-  for (let glob of typescriptSources) {
-    let typescriptOptions = {
-      module: 'commonjs',
-      moduleResolution: 'node'
-    };
-    if (glob.split('.')[1] === 'tsx') {
-      typescriptOptions.jsx = 'react';
-    }
-
-    gulp.src(glob)
-      .pipe(plugins.sourcemaps.init())
-      .pipe(plugins.typescript(typescriptOptions))
-      .pipe(plugins.sourcemaps.write())
-      .pipe(gulp.dest('src/'));
-  }
-});
-gulp.task('typescript:compile:watch', ['set-watching'], function() {
-  gulp.watch(typescriptSources, ['typescript:compile']);
-});
-
-gulp.task('typescript', ['typescript:compile']);
-gulp.task('typescript:watch', ['set-watching'], function() {
-  gulp.watch(typescriptSources, ['typescript'])
-});
-
-// JSX and ES6
-
-let jsxSources = 'src/**/*.jsx';
-
-gulp.task('jsx:compile', function() {
-  gulp.src(jsxSources)
-    .pipe(plugins.sourcemaps.init())
-    .pipe(plugins.babel({whitelist: ['react', 'es6.blockScoping', 'es6.spread']}))
-    .pipe(plugins.sourcemaps.write())
-    .pipe(gulp.dest('src/'));
-});
-gulp.task('jsx:watch', ['set-watching'], function() {
-  gulp.watch(jsxSources, ['jsx:compile']);
-});
-
-gulp.task('jsx', ['jsx:compile']);
-gulp.task('jsx:watch', ['set-watching'], function() {
-  gulp.watch(jsxSources, ['jsx'])
-});
-
-// SASS
-
-let sassSources = 'src/**/*.scss';
-
-gulp.task('sass:compile', function() {
-  gulp.src(sassSources)
-    .pipe(plugins.sourcemaps.init())
-    .pipe(plugins.sass({'outputStyle': 'expanded'}).on('error', plugins.sass.logError))
-    .pipe(plugins.autoprefixer(['> 1%', 'last 2 version']))
-    .pipe(plugins.sourcemaps.write())
-    .pipe(gulp.dest('src/'));
-});
-gulp.task('sass:compile:watch', ['set-watching'], function() {
-  gulp.watch(sassSources, ['sass:compile']);
-});
-
-gulp.task('sass:lint', function() {
-  gulp.src(sassSources)
-    .pipe(plugins.scssLint({
-      endless: watching
-    }))
-    .pipe(plugins.scssLint.failReporter());
-});
-gulp.task('sass:lint:watch', ['set-watching'], function() {
-  gulp.watch(sassSources, ['sass:lint']);
-});
-
-gulp.task('sass', ['sass:compile', 'sass:lint']);
-gulp.task('sass:watch', ['set-watching'], function() {
-  gulp.watch(sassSources, ['sass'])
+    // Start a webpack-dev-server
+    new WebpackDevServer(webpack(myConfig), {
+        publicPath: "/" + myConfig.output.publicPath,
+        stats: {
+            colors: true
+        }
+    }).listen(8080, "localhost", function(err) {
+        if(err) throw new gutil.PluginError("webpack-dev-server", err);
+        gutil.log("[webpack-dev-server]", "http://localhost:8080/webpack-dev-server/index.html");
+    });
 });
